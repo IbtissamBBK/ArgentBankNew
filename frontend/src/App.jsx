d0ecb94a90d553244a745d6ba5f4a35ebf736fc3
Ibtissam <benbourekibtissam@gmail.com>
2024-12-22 21:15:26 +0100

    add protectedroute for user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import SignInPage from './pages/SignInPage.jsx';
 import UserPage from './pages/UserPage.jsx';
+import ProtectedRoute from "./components/ProtectedRoute";
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 
@@ -13,7 +14,15 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<SignInPage />} />
-        <Route path="/user" element={<UserPage />} />
+         {/* Utilisation de ProtectedRoute pour sécuriser la page user */}
+         <Route
+          path="/user"
+          element={
+            <ProtectedRoute>
+              <UserPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
       <Footer />
     </>
